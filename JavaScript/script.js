e63a1fa13f7453cebf32122941ceca69274d9ba1
BigBoyFizzy <elias.elmefors@gmail.com>
2026-05-29 14:40:28 +0200

    two first rows

---
+++ JavaScript/script.js
@@ -0,0 +1,14 @@
+// console.log("Det fiúnkar!!")
+
+// document.addEventListener("keydown", (event) => {
+
+//     console.log(event.key)
+
+//     if(event.key == "b"){
+//         document.getElementById("Elias").style = "color:blue";
+//     }
+//     else{
+//         document.body.style = "background-color:red";
+//     }
+
+// });
