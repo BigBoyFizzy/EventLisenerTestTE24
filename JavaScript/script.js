caa29a556f7abf416eb66fa0b4620f9510827f2b
BigBoyFizzy <elias.elmefors@gmail.com>
2026-06-03 16:37:58 +0200

    Muhhhhhhaaahhahahahahahha

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,11 +1,26 @@
  console.log("Det fiúnkar!!")
 
- document.addEventListener("keypress", (event) => {
+ document.addEventListener("keydown", (event) => {
 
      console.log(event);
 
-if(event.key == "w"){
-         document.getElementById("w").style = "color:blue";
-     }
+
+    document.getElementById(event.code).style = "background-color:red";
+     
+    if( event.code == "Enter"){
+        document.getElementById("lowE").style = "background-color:red";
+    }
+});
+
+document.addEventListener("keyup", (event) => {
+
+     console.log(event);
+
+
+    document.getElementById(event.code).style = "background-color:black";
+     if( event.code == "Enter"){
+        document.getElementById("lowE").style = "background-color:black";
+    }
 
 });
+
